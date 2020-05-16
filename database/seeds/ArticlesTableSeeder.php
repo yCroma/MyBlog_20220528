82bcfbbbe99deb8a912a2d3af2e3c70de15b3a08
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-17 03:21:42 +0900

    Fix ArticlesTableSeeder
    最初のが実行できなかったので、動くように修正。

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,11 +11,13 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        @for ($i = 1; $i < 11; $i++)
-            # 10件のダミーデータを作成する
-            $title = "{{ $i }}件目の投稿";
-            $draft = "{{ $i }}番目の記事です。";
+        $title = "記事のタイトル";
+        $draft = "記事の原稿";
+        for ($i = 1; $i < 11; $i++){
+            // 10件のダミーデータを作成する
+            $title = "{{ $i }}"."件目の投稿";
+            $draft = "{{ $i }}"."番目の記事です。";
             App\Article::create(['title' => $title, 'draft' => $draft]);
-        @endfor
+        };
     }
 }
