24889c1acae14139e5cf714aa5a2485b3f653cf7
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-17 01:47:19 +0900

    Create migration & table tag_article
    TagとArticleの中間テーブルとmigrationを作成しました。

---
+++ database/migrations/2020_05_16_164503_create_tag_article_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_article', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_article');
+    }
+}
