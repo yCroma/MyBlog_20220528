f7f6c2f784b17f748cc59c69a6b86a3bfed275dc
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-19 02:09:46 +0900

    Create edit.blade.php
    Article の編集画面を作成した。

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{ $article->title }}の編集</title>
+</head>
+<body>
+  <form action="{{ route('articles.update', ['article' => $article->id]) }}" method="post">
+    @csrf
+    @method('put')
+    <input type="text" name="title" value="{{ $article->title }}">
+    <br>
+    <textarea name="draft" cols="30" rows="10" required>
+      {{ $article->draft }}
+    </textarea>
+    <br>
+    <button type="submit">投稿</button>
+  </form>
+</body>
+</html>
