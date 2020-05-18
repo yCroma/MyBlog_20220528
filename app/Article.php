901aa44cbd3a11dfbc0892818ee2b17437dae5d4
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-19 00:20:21 +0900

    Add fillable
    コントローラーで fill を使えるようにするために追記。

--- app/Article.php
+++ app/Article.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    protected $fillable = ['title', 'draft'];
+
     public function tags()
     {
         return $this->belongsToMany('App\Tag');
