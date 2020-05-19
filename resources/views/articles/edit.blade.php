45f5ac9e75ca67c0bda16092f794834f1a757b8f
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-19 23:49:37 +0900

    Update edit.blade.php
    Article の edit 画面にMarkdownエディターを実装。

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -11,9 +11,14 @@
     @method('put')
     <input type="text" name="title" value="{{ $article->title }}">
     <br>
-    <textarea name="file_draft" cols="30" rows="10" required>
-      {{ $draft }}
-    </textarea>
+    <textarea id="editor" name="file_draft" cols="30" rows="10" required>{{ $draft }}</textarea>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+    <script>
+      var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+    </script>
+
     <br>
     <button type="submit">投稿</button>
   </form>
