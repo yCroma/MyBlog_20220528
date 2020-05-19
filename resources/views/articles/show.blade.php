46618f6b376e9d31577d28f7980d33d5c7fe12a3
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-20 01:40:44 +0900

    Remove escape $draft
    パースされたHTMLを表示するためにエスケープを外した。

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -10,8 +10,8 @@
   <div>
   <p>{{ $article->created_at }}</p><br>
   <hr>
-  <p>{!! nl2br(e($draft)) !!}</p>
-  </div>
+  <p>{!! $draft !!}</p>
+  <hr>
   <p><a href="{{ route('articles.index') }}"><< 記事一覧へ戻る</a></p>
 </body>
 </html>
