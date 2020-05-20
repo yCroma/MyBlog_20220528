f49175033dfc2d4596b83e4ef418cdb93d1968f3
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-20 18:26:11 +0900

    Add Index Tag
    Tag の index ページ用のコントローラーを用意した。

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -14,7 +14,8 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::all();
+        return view('tags.index', ['tags' => $tags]);
     }
 
     /**
