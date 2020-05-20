0f8c5f25a2dcb483d97fe76c022f48208380496d
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-20 22:55:19 +0900

    Update edit.blade.php
    編集画面で関連づけたタグと登録ずみのタグを登録できる処理を実装。

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -9,8 +9,24 @@
   <form action="{{ route('articles.update', ['article' => $article->id]) }}" method="post">
     @csrf
     @method('put')
+    <!-- 記事のタイトルを表示 -->
     <input type="text" name="title" value="{{ $article->title }}">
     <br>
+    <!-- 記事に関連づけられたタグを表示 -->
+    @forelse ($article_tags as $article_tag)
+      {{ $article_tag }}
+    @empty
+    <div>タグ未登録</div>
+    @endforelse
+    <br>
+    <!-- 登録されているタグを表示 -->
+    @forelse ($tags as $tag)
+      <input type="checkbox" name="tags[]" value="{{ $tag->id }}">{{ $tag->name }}
+    @empty
+    <div>タグ未登録</div>
+    @endforelse
+    <br>
+    <!-- エディター -->
     <textarea id="editor" name="file_draft" cols="30" rows="10" required>{{ $draft }}</textarea>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
