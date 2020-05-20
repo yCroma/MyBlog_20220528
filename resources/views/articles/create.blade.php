e849deaa342ace84ab9f311963899d1184f9fe15
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-20 22:51:55 +0900

    Update create.blade.php
    コントローラーで用意した、タグを表示する処理を実装。

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -11,6 +11,14 @@
     @csrf
     <input type="text" name="title" placeholder="タイトル">
     <br>
+    <p>タグをつける</p>
+    <br>
+    @forelse ($tags as $tag)
+    <input type="checkbox" name="tags[]" value="{{ $tag->id }}">{{ $tag->name }}
+    @empty
+    <p>登録されているタグはありません</p>
+    @endforelse
+    <br>
     <textarea id="editor" name="draft" cols="30" rows="10"></textarea>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
