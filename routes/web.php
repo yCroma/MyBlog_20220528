d6bb6a12c8a9cc4eb17b7fedee75f222a74869d3
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-22 15:11:41 +0900

    Update web.php
    home のルートを使わないためコメントアウト。

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,4 @@ Route::resource('admin', 'AdminController')->only([
 Route::resource('articles', 'ArticleController')->middleware('auth');
 Route::resource('tags', 'TagController')->middleware('auth');
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
