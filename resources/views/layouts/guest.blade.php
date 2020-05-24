575dd187702127d184d8a63fda4a5558fcbe972c
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-25 04:53:45 +0900

    Update guest.blade.php
    ゲストページに認証で見た目が変わる処理を実装。

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -26,16 +26,31 @@
   <div id="container">
     <!-- ヘッダー -->
     <div class="row">
-      <header class="col-12 bg-dark">
+      <header class="col-12 bg-dark text-center">
         <a href="/guest">
-          <h1 class="display-2 text-center text-white m-2">
+          <h1 class="display-2 text-white m-2">
             Cromalog
           </h1>
         </a>
-        <p class="text-center text-white m-2">
+        @auth
+        <h5 class="text-white m-2">
+          現在、Adminでログインしています。
+        </h5>
+        <a href="{{ route('logout') }}" onclick="event.preventDefault(); 
+          document.getElementById('logout-form').submit();">
+          <i class="fas fa-sign-out-alt text-white">
+            Logout
+          </i>
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+          @csrf
+        </form>
+        @else
+        <p class="text-white m-2">
           勉強したものや、あとで振り返りたいもの書いていきます。<br>
           当分はこれまでに書きためたやつを載せていきます。
         </p>
+        @endauth
       </header>
     </div>
     <!-- コンテンツ -->
@@ -44,11 +59,17 @@
     </div>
     <!-- フッター -->
     <div class="row">
-      <footer class="col-12 bg-primary" style="height:80px;">
+      <footer class="col-12 bg-primary" style="height:105px;">
         <p class="text-center mt-4">
           <a href="https://github.com/yCroma" class="text-dark">
             <i class="fab fa-github fa-2x" style="size: 7x;">
               yCroma
+            </i>
+          </a>
+          <br>
+          <a href="{{ route('login') }}" class="pt-1">
+            <i class="fas fa-sign-in-alt">
+              ログインはこちら
             </i>
           </a>
         </p>
