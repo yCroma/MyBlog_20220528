a78b073d00405905fd052e77f9583a0e1afbbcb7
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-24 22:39:04 +0900

    Create guest.blade.php
    継承の元となる、viewを作成した。

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div id="container">
+    <!-- ヘッダー -->
+    <div class="row">
+      <header class="col-12 bg-dark">
+        <h1 class="display-2 text-center text-white m-2">
+          Cromalog
+        </h1>
+        <p class="text-center text-white m-2">
+          勉強したものや、あとで振り返りたいもの書いていきます。<br>
+          当分はこれまでに書きためたやつを載せていきます。
+        </p>
+      </header>
+    </div>
+    <!-- コンテンツ -->
+    <div class="row">
+      @yield('contents')
+    </div>
+    <!-- フッター -->
+    <div class="row">
+      <footer class="col-12 bg-info">
+        <h1 class="display-2 text-center text-white m-2">
+          フッター
+        </h1>
+      </footer>
+    </div>
+  </div>
+</body>
+</html>
