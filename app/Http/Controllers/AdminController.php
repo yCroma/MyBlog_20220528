7c380e7830dc798fc29ffba5a34c42b023101947
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-25 04:52:34 +0900

    Update AdminController.php
    Adminのindexページで継承するために、タグのリクエストを作成。

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tag;
 
 class AdminController extends Controller
 {
@@ -13,7 +14,8 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        $view_tags = Tag::all();
+        return view('admin.index', ['view_tags' => $view_tags]);
     }
 
     /**
