9b823242ef00f2ca6feebbfb8be0634bb338d55b
yCroma <58209438+yCroma@users.noreply.github.com>
2020-05-25 22:23:26 +0900

    Update show.blade.php
    Adminのタグ検索ページのbladeに継承を実装。

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -1,28 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ $tag->name }}の記事一覧</title>
-</head>
-<body>
-  <h1>{{ $tag->name }}の記事一覧</h1>
-  <div>
-    <table>
-      @forelse ($articles as $article)
-      <tr>
-        <td>
-          <a href="{{ route('articles.show', ['article' => $article->id]) }}">
-            {{ $article->title }}
-          </a>
-        </td>
-      </tr>
+@extends('layouts.guest_main')
+
+@section('page_title')
+  {{ $tag->name }}の記事一覧
+@endsection
+
+@section('main')
+  <ul class="list-group mb-2">
+    <li class="list-group-item mt-2">
+      <h2 class="mt-3"> 
+          {{ $tag->name }}の記事一覧
+      </h2>
+    </li>
+    @forelse ($articles as $article)
+    <li class="list-group-item">
+      <p>投稿日:{{ $article->created_at }}</p>
+      <a href="{{ route('articles.show', ['article' => $article->id]) }}">
+        <h3 class="text-dark">
+          {{ $article->title }}
+        </h3>
+      </a>
+      <br>
+      <div class="text-right">
+      @forelse ($article->tags as $tag)
+        <a class="badge badge-secondary" href="{{ route('tags.show', ['tag' => $tag->id]) }}">
+          {{ $tag->name }}
+        </a>
       @empty
-      <tr>
-        <td>{{ $tag->name }}の記事はありません。</td>
-      </tr>
+        <p>タグ未登録</p>
       @endforelse
-    </table>
-  </div>
-</body>
-</html>
+      </div>
+    </li>
+    @empty
+    <li class="list-group-item mt-2">
+      投稿されている記事はありません
+    </li>
+    @endforelse
+  </ul>
+@endsection
